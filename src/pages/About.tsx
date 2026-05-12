350cb1716d064578c666538827700ce142c46a31
MarkRywell <gopro855@gmail.com>
2026-05-12 20:00:54 +0800

    refactor: add new section on about us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <>
-      <div className="min-h-screen w-full flex flex-col">
+      <div className="min-h-screen w-full flex flex-col bg-gray-800 ">
         <div className="relative w-full overflow-hidden p-10 md:p-15">
           
           <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1778574293/rccgy/running7_unc7ac.jpg" alt="Running Group" className="absolute inset-0 h-full w-full object-cover object-[50%_20%]" />
@@ -15,8 +15,8 @@ function About() {
           </div>
         </div>
 
-        <div className="w-full h-full flex flex-col justify-center items-center bg-gray-800 text-white py-10 px-5 sm:px-10 xl:px-30">
-          <div className="w-full flex flex-col md:flex-row justify-center items-center">
+        <div className="w-full h-full flex flex-col justify-center items-center text-white py-10">
+          <div className="w-full flex flex-col md:flex-row justify-center items-center px-5 sm:px-10 xl:px-30">
             <div className="w-full md:w-1/2">
             <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1778574438/rccgy/banner_bwmkzd.jpg" alt="Banner" className="w-full h-full object-cover rounded-lg" />
               <div className="hidden md:block lg:hidden my-5">
@@ -40,8 +40,13 @@ function About() {
             </div>
           </div>
 
-          <div>
-            
+          <div className="w-full px-10 sm:px-10 xl:px-30 mt-5 py-5 flex flex-col md:flex-row md:gap-10 justify-center items-center bg-gray-600">
+            <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1777606832/rccgy/running_d3ueam.jpg" alt="Runner" className="w-full h-full md:hidden object-cover rounded-lg mb-5" />
+            <div className="w-full md:w-1/2">
+              <h1 className="text-2xl font-bold my-5 text-start">THE MOVEMENT</h1>
+              <p>Today, RCCGY is a synchronized movement. We have transformed the pavement into a stage for excellence, bringing together athletes, dreamers, and partners. From the first light of dawn to the city's night glow, our presence is felt across the city.</p>
+            </div>
+            <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1777606832/rccgy/running_d3ueam.jpg" alt="Runner" className="w-full h-full md:block md:w-2/4 lg:w-1/3 md:h-1/3 md:object-cover rounded-lg mb-5" />
           </div>
 
         </div>

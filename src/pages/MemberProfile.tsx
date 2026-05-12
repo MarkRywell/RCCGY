4e21c75136142eaa72a0af7d0c2787920aea971e
MarkRywell <gopro855@gmail.com>
2026-05-12 11:17:23 +0800

    refactor: update member profile UI

--- src/pages/MemberProfile.tsx
+++ src/pages/MemberProfile.tsx
@@ -66,19 +66,17 @@ function MemberProfile() {
 
     return (
         <>
-            <div className="min-h-screen w-full flex flex-col bg-gray-800">
-                <div className="min-h-52 w-full flex items-center justify-center p-10">
+            <div className="min-h-screen w-full bg-gray-800 text-white text-center flex flex-col">
+                <div className="min-h-52 w-full flex flex-col items-center justify-center p-5 gap-5">
                     <img
                         src={profileImage}
                         className="w-52 h-52 object-cover rounded-full"
                         alt={`${name} profile`}
                     />
-                </div>
 
-                <div className="w-full mx-auto p-6 text-white text-center flex flex-col gap-4">
                     <h1 className="text-3xl font-bold">{name}</h1>
                     <h2 className="text-xl text-secondary font-semibold">{role.toUpperCase()}</h2>
-                    <div className="flex flex-col gap-2">
+                    <div className="flex flex-col gap-2 w-full px-5 md:w-1/2 lg:w-1/3">
                         <div className="text-lg flex justify-between items-center">
                             <p>Phone</p>
                             <p>{phone || "N/A"}</p>
@@ -88,8 +86,10 @@ function MemberProfile() {
                             <p>{email || "N/A"}</p>
                         </div>
                     </div>
+                </div>
 
-                    <div className="mt-6">
+                <div className="w-full mx-auto p-6 flex flex-col gap-4">
+                    <div>
                         <h3 className="text-2xl font-semibold mb-4">Personal Records</h3>
                         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
                             <div className="bg-gray-700 p-4 rounded">

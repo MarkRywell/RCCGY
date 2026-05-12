609bc71a30e9300e4566d728ccceedd2f7be0ede
MarkRywell <gopro855@gmail.com>
2026-05-12 19:18:58 +0800

    refactor: update responsiveness of About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -16,11 +16,18 @@ function About() {
         </div>
 
         <div className="w-full h-full flex flex-col justify-center items-center bg-gray-800 text-white py-10 px-5 sm:px-10 xl:px-30">
-          <div className="w-full flex flex-col md:flex-row">
-            <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1778574438/rccgy/banner_bwmkzd.jpg" alt="Banner" className="w-full md:w-1/2 h-full object-cover" />
+          <div className="w-full flex flex-col md:flex-row justify-center items-center">
+            <div className="w-full md:w-1/2">
+            <img src="https://res.cloudinary.com/di8bd6f96/image/upload/v1778574438/rccgy/banner_bwmkzd.jpg" alt="Banner" className="w-full h-full object-cover rounded-lg" />
+              <div className="hidden md:block lg:hidden my-5">
+                <p className="text-sm text-gray-300 font-semibold">WELCOME TO RUNCLUB:</p>
+                <h1 className="text-4xl font-bold my-5">RANNN CREW CGY</h1>
+              </div>
+            </div>
+            
             <div className="w-full md:w-1/2 flex flex-col gap-5 p-5 md:p-10">
-              <p className="text-sm text-gray-300 font-semibold">WELCOME TO RUNCLUB:</p>
-              <h1 className="text-4xl font-bold">RANNN CREW CGY</h1>
+              <p className="text-sm text-gray-300 font-semibold block md:hidden lg:block">WELCOME TO RUNCLUB:</p>
+              <h1 className="text-4xl font-bold block md:hidden lg:block">RANNN CREW CGY</h1>
               <p className="border-b pb-5">In the quiet streets of <span className="font-semibold">Puntod, Cagayan de Oro</span>, a small group of strangers found a common rhythm. What began as a humble effort to stay active evolved into the grit of finishing the next kilometer together. We didn't just start a club; we sparked a change.</p>
             
               <div className="flex w-full items-center gap-5">
